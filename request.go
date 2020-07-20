29a21525a805ba2be1d68b3973d843841b3739e7
Masashi Salvador Mitsuzawa <masashisalvador57f@gmail.com>
2020-07-20 20:19:01 +0900

    format

--- request.go
+++ request.go
@@ -2,9 +2,9 @@ package cloudsign
 
 // PostDocumentRequest is request object for post document api.
 type PostDocumentRequest struct {
-	title string
-	note string
-	message string
-	templateID string
+	title       string
+	note        string
+	message     string
+	templateID  string
 	canTransfer bool
 }
